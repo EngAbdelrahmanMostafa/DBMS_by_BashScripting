a35da7e107b26d5658d45c47d1b39fa0495d0188
“EngAbdelrahmanMostafa” <eng.abdelrahman.mostafa.soliman@gmail.com>
2022-02-15 11:17:48 +0200

    modify loop in home menu

--- HomeMenu.sh
+++ HomeMenu.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 # dataBase Home Menu
 echo "Welcome To The DBMS"
+flag=true;
+while $flag ; do
+
 PS3="what is the action you want to perform on the data base "
 	select i in "Create DataBase" "List DataBases " "Connect to DataBase"  "Rename DataBase" "Drop Data Base" " Sign Out!"
 	do
@@ -10,8 +13,10 @@ PS3="what is the action you want to perform on the data base "
     3) DBScripts/./DBConn.sh ;;
     4) DBScripts/./renameDB.sh ;;
 	  5) DBScripts/./DBDrop.sh ;;
-	  6) break; ;;
-
+	  6) flag=false;
+				break;
+				exit; ;;
 	  esac
-		exit;
+
 	done
+done
